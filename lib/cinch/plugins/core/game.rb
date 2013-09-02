caitlin/cinch-resistancegame

commit 7ff045bef19da4df4280284750143718e2a849a4

extract a few Lancelot things into own function

lib/cinch/plugins/core/game.rb
@@ -226,6 +226,13 @@ class Game
     self.lancelot_deck.count(:switch) % 2 == 1
   end
 
+  def play_lancelot_card
+    return unless self.variants.include?(:lancelot1) && @current_count.number > 2
+
+    @current_round.lancelot_card = self.lancelot_deck.pop
+    self.switch_lancelots if @current_round.lancelots_switch?
+  end
+
   def switch_lancelots
     good_lancelot = self.find_player_by_role(:good_lancelot)
     evil_lancelot = self.find_player_by_role(:evil_lancelot)
@@ -374,14 +381,7 @@ class Game
     @current_round = Round.new(new_round)
     self.rounds << @current_round
     self.assign_team_leader
-    if self.variants.include?(:lancelot1)
-      if @current_round.number > 2
-        @current_round.lancelot_card = self.lancelot_deck.pop
-        if @current_round.lancelots_switch?
-          self.switch_lancelots
-        end
-      end
-    end
+    self.play_lancelot_card
   end
 
 

lib/cinch/plugins/resistance_game.rb
@@ -873,28 +873,33 @@ module Cinch
 
       def start_new_round
         @game.start_new_round
-        unless @game.current_round.lancelot_card.nil?
-          if @game.current_round.lancelots_switch?
-            Channel(@channel_name).send "LANCELOTS: Switch!"
-            evil_lancelot = @game.find_player_by_role(:evil_lancelot)
-            good_lancelot = @game.find_player_by_role(:good_lancelot)
-            good_loyalty_msg = "LANCELOTS SWITCH: You are now a member of the RESISTANCE."
-            evil_loyalty_msg = "LANCELOTS SWITCH: You are now a SPY."
-            if @game.lancelots_switched?
-              User(good_lancelot.user).send evil_loyalty_msg
-              User(evil_lancelot.user).send good_loyalty_msg
-            else 
-              User(good_lancelot.user).send good_loyalty_msg
-              User(evil_lancelot.user).send evil_loyalty_msg
-            end
-          
-          else
-            Channel(@channel_name).send "LANCELOTS: No switch."
-          end
-        end
+        self.show_lancelot_card
+
         two_fail_warning = (@game.current_round.special_round?) ? " This mission requires TWO FAILS for the spies." : ""
         Channel(@channel_name).send "MISSION #{@game.current_round.number}. Team Leader: #{@game.team_leader.user.nick}. Please choose a team of #{@game.current_team_size} to go on the mission.#{two_fail_warning}"
         User(@game.team_leader.user).send "You are team leader. Please choose a team of #{@game.current_team_size} to go on the mission. \"!team#{team_example(@game.current_team_size)}\""
+      end
+
+      def show_lancelot_card
+        return if @game.current_round.lancelot_card.nil?
+
+        if @game.current_round.lancelots_switch?
+          Channel(@channel_name).send "LANCELOTS: Switch!"
+          evil_lancelot = @game.find_player_by_role(:evil_lancelot)
+          good_lancelot = @game.find_player_by_role(:good_lancelot)
+          good_loyalty_msg = "LANCELOTS SWITCH: You are now a member of the RESISTANCE."
+          evil_loyalty_msg = "LANCELOTS SWITCH: You are now a SPY."
+          if @game.lancelots_switched?
+            User(good_lancelot.user).send evil_loyalty_msg
+            User(evil_lancelot.user).send good_loyalty_msg
+          else
+            User(good_lancelot.user).send good_loyalty_msg
+            User(evil_lancelot.user).send evil_loyalty_msg
+          end
+
+        else
+          Channel(@channel_name).send "LANCELOTS: No switch."
+        end
       end
 
       def team_leader_prompt

commit 938586acffb4dbafa704859487d405653808837f

Fix lancelot1 typo
Broke this in 7ff045bef19da4df4280284750143718e2a849a4, sorry

lib/cinch/plugins/core/game.rb
@@ -238,7 +238,7 @@ class Game
   end
 
   def play_lancelot_card
-    return unless self.variants.include?(:lancelot1) && @current_count.number > 2 || self.variants.include?(:lancelot2)
+    return unless self.variants.include?(:lancelot1) && @current_round.number > 2 || self.variants.include?(:lancelot2)
 
     @current_round.lancelot_card = self.lancelot_deck.pop
     self.switch_lancelots if @current_round.lancelots_switch?